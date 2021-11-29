85e5e73375ae1a2c2bb0d9319b3846d4c6adfecb
Mateusz Staniak <mtst@mstaniak.pl>
2021-11-29 18:01:43 +0100

    updated feature-level noise sim

--- R/feature_level.R
+++ R/feature_level.R
@@ -1,6 +1,6 @@
 #' @export
-simulate_unique_peptides = function(protein_profiles, num_unique, channel_variation,
-                                    noise_sd = 0.1, y_axis_noise_sd = 1.5) {
+simulate_unique_peptides = function(protein_profiles, num_unique, noise_sd,
+                                    y_axis_range) {
   psms_unique = data.table::rbindlist(
     lapply(split(protein_profiles, protein_profiles$Protein, drop = FALSE),
            function(protein_profile) {
@@ -11,13 +11,10 @@ simulate_unique_peptides = function(protein_profiles, num_unique, channel_variat
              }))
            })
   )
-  psms_unique[, Log2Intensity := Abundance + rnorm(1, sd = y_axis_noise_sd) + rnorm(.N, sd = noise_sd),
+  psms_unique[, Log2Intensity := Abundance + runif(1, y_axis_range[1],
+                                                   y_axis_range[2]) + rnorm(.N, sd = noise_sd),
               by = c("Protein", "PSM")]
-  psms_unique$Log2Intensity = psms_unique$Log2Intensity + channel_variation[psms_unique$RunChannel]
   psms_unique[, PSM := paste(PSM, as.numeric(Protein), sep = "")]
-  psms_unique[, MixCond := NULL]
-  psms_unique[, ProtCond := NULL]
-  psms_unique[, IsSignificant := NULL]
   psms_unique[, Abundance := NULL]
   data.table::setnames(psms_unique, "Protein", "ProteinName")
   psms_unique
@@ -25,8 +22,8 @@ simulate_unique_peptides = function(protein_profiles, num_unique, channel_variat
 
 #' @export
 simulate_shared_profiles = function(profiles, combinations, num_shared,
-                                    weights, channel_variation, noise_sd = 0.1,
-                                    y_axis_noise_sd = 1.5) {
+                                    weights, noise_sd,
+                                    y_axis_range) {
   proteins = as.character(unique(profiles$Protein))
   channels = unique(profiles$Channel)
   shared_profiles = vector("list", nrow(combinations))
@@ -46,15 +43,14 @@ simulate_shared_profiles = function(profiles, combinations, num_shared,
     shared_profile = data.table::data.table(Protein = "0",
                                             Channel = channels,
                                             Abundance = shared_profile[, 1])
-    shared_annot = unique(profiles[, .(Run, Mixture, TechRepMixture, RunChannel,
-                                       Channel, BioReplicate, NestedBioRep, Condition)])
+    shared_annot = unique(profiles[, .(Run, Mixture, TechRepMixture,
+                                       Channel, BioReplicate, Condition)])
     shared_profile = merge(shared_annot, shared_profile, by = "Channel")
 
     shared_profiles_i = suppressWarnings(simulate_unique_peptides(shared_profile,
                                                                   num_shared,
-                                                                  channel_variation,
                                                                   noise_sd,
-                                                                  y_axis_noise_sd))
+                                                                  y_axis_range))
     shared_profiles_i[, PSM := as.numeric(as.factor(PSM))]
     protein_ids = paste(proteins[combination], sep = "_", collapse = "_")
     shared_profiles_i[, PSM := paste("PS", PSM, protein_ids, sep = "_")]
@@ -72,12 +68,17 @@ simulate_shared_profiles = function(profiles, combinations, num_shared,
 
 #' @export
 combine_features = function(unique_features, shared_features) {
+  if (nrow(unique_features) > 0 | nrow(shared_features) > 0) {
+    charge = 1
+  } else {
+    charge = numeric(0)
+  }
   all_features = rbind(unique_features, shared_features)
   all_features[, Intensity := 2 ^ Log2Intensity]
   all_features[, PSM := gsub("_", "", PSM)]
   all_features[, PeptideSequence := PSM]
   all_features[, PSM := paste(PSM, 1, sep = "_")]
   all_features[,
-               list(ProteinName, PeptideSequence, Charge = 1, PSM, Run, Mixture,
-                    TechRepMixture, Channel, BioReplicate, Condition, Intensity)]
+               list(ProteinName, PeptideSequence, Charge = charge, PSM, Run, Mixture,
+                    TechRepMixture, Channel, BioReplicate, Condition, Log2Intensity, Intensity)]
 }
